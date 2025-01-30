3ec4dd75594dc730d909a88e7107a32496fefdae
Abhishek Raj Chauhan <arc805@gmail.com>
2025-01-31 00:53:16 +0530

    Added todo item delete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,45 @@
 <script lang="ts">
 	let { data } = $props();
-	let content = $state('');
 
 	async function getToDoItem(toDoItemId: string) {
 		const response = await fetch(`/toDoItem/${toDoItemId}`, {
 			method: 'GET'
 		});
 		const { toDoItem } = await response.json();
-		console.log('toDoItem', toDoItem);
 		return toDoItem;
 	}
 
-	async function addToDoItem() {
-		const response = await fetch('/toDoItem', {
-			method: 'POST',
-			body: JSON.stringify({ content }),
-			headers: {
-				'Content-Type': 'application/json'
-			}
+	async function addToDoItem(event: Event) {
+		event.preventDefault();
+		const formData = new FormData(event.target as HTMLFormElement);
+		const content = formData.get('content')?.toString().trim() ?? '';
+		if (content.length > 0) {
+			const response = await fetch('/toDoItem', {
+				method: 'POST',
+				body: JSON.stringify({ content }),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			const { toDoItemId } = await response.json();
+			const toDoItem = await getToDoItem(toDoItemId);
+			const toDoItems = [...data.toDoItems, toDoItem];
+			data = { ...data, toDoItems };
+		}
+	}
+
+	async function deleteToDoItem(toDoItemId: string) {
+		console.log('Delete', toDoItemId);
+		const response = await fetch(`/toDoItem/${toDoItemId}`, {
+			method: 'DELETE'
 		});
-		content = '';
-		const { toDoItemId } = await response.json();
-		const toDoItem = await getToDoItem(toDoItemId);
-		const toDoItems = [...data.toDoItems, toDoItem];
-		data = { ...data, toDoItems };
+		const status = response.status;
+		if (status === 204) {
+			const toDoItems = data.toDoItems.filter(
+				(toDoItem: { id: string }) => toDoItem.id !== toDoItemId
+			);
+			data = { ...data, toDoItems };
+		}
 	}
 </script>
 
@@ -34,7 +50,7 @@
 
 <div>
 	<form onsubmit={addToDoItem}>
-		<input type="text" name="content" autocomplete="off" required bind:value={content} />
+		<input type="text" name="content" autocomplete="off" required />
 		<button type="submit">Add</button>
 	</form>
 </div>
@@ -42,7 +58,10 @@
 <div>
 	<ul>
 		{#each data.toDoItems as toDoItem (toDoItem.id)}
-			<li>{toDoItem.id} : {toDoItem.content} : {toDoItem.status}</li>
+			<li>
+				<span>{toDoItem.id}:{toDoItem.content}:{toDoItem.status}</span>
+				<button onclick={() => deleteToDoItem(toDoItem.id)}>Delete</button>
+			</li>
 		{/each}
 	</ul>
 </div>
--- src/routes/toDoItem/[id]/+server.ts
+++ src/routes/toDoItem/[id]/+server.ts
@@ -4,10 +4,13 @@ import { json } from '@sveltejs/kit';
 export async function GET({ params, cookies }) {
 	const toDoItemId = params.id;
 	const userId = cookies.get('userId') ?? 'defaultUserId';
-	console.log(
-		'Returning the following toDoItem: ',
-		ToDoListService.getInstance().getToDoItem(userId, toDoItemId)
-	);
 	const toDoItem = ToDoListService.getInstance().getToDoItem(userId, toDoItemId);
 	return json({ toDoItem }, { status: 200 });
+}
+
+export async function DELETE({ params, cookies }) {
+	const toDoItemId = params.id;
+	const userId = cookies.get('userId') ?? 'defaultUserId';
+	ToDoListService.getInstance().removeToDoItem(userId, toDoItemId);
+	return new Response(null, { status: 204 });
 }
